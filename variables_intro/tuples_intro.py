5c58a52ba685095b8d2b99739f5db028bbb97a26
Munir Sharif <munirsharif463@gmail.com>
2022-05-19 15:41:50 +0100

    tuples_intro.py

---
+++ variables_intro/tuples_intro.py
@@ -0,0 +1,7 @@
+anime = ("Dragon Ball Z", "One Piece", "Hunter X Hunter")
+print(anime, type(anime))
+print(anime[-1])
+# Tuples are IMMUTABLE
+# anime[0] = "Yi-Gi-Oh" # This doesn't work
+print(anime.count("One Piece"))
+print(anime.index("Hunter X Hunter"))
