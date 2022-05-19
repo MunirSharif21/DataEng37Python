2d8695115f74181e0ed007871d44031b709440d1
Munir Sharif <munirsharif463@gmail.com>
2022-05-19 15:36:58 +0100

    Finished input_intro.py

--- variables_intro/input_intro.py
+++ variables_intro/input_intro.py
@@ -19,6 +19,44 @@ print(text.isupper())  # check if ALL LETTERS are uppercase
 print(text.islower())  # check if ALL LETTERS are lowercase
 print(text.endswith("!"))  # check if string ends with specified value
 print(text.startswith("H"))  # check if string begins with specified value
-"""
+
 x = None
 print(x is None)
+"""
+
+shopping_list = ["Bread", "Bananas", "Biscuits", "Oat Milk"]
+print(shopping_list, type(shopping_list))
+
+# INDEXING AND SLICING
+
+print(shopping_list[0])
+print(shopping_list[-1])
+print(shopping_list[::-1])
+
+# Lists ARE MUTABLE
+
+shopping_list[0] = "Sugar"
+print(shopping_list)
+shopping_list.append("Milk")
+print(len(shopping_list))
+shopping_list.remove("Bananas")
+# only the first instance of bananas will be removed
+# if there are multiple bananas in the list
+
+hi = "Hello"
+print(hi.upper())
+print(hi)
+# upper method only returns something
+# the original string won't be changed
+
+print(shopping_list.pop())
+# removes item from list and gives it you
+# by default, last item will be removed
+# unless you provide an index.
+print(shopping_list)
+
+mixed_list = [1, 2, 3.0, 4+5j, "greetings", True, False, None]
+print(mixed_list)
+
+nested_list = [[1, 2], [3, 4]]
+print(nested_list[1][-1])  # second list, last item (4)
