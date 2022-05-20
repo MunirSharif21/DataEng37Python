f08a341c094f442ca7d9d6abfa01a39027418dcb
Munir Sharif <munirsharif463@gmail.com>
2022-05-20 13:46:05 +0100

    created dictionary_intro.py

--- variables_intro/dictionary_intro.py
+++ variables_intro/dictionary_intro.py
@@ -1,5 +1,5 @@
 # Dictionary (Key:Value Pairs)
-
+"""
 data_eng_37 = {
     "course_name": "Data Engineering 37",
     "client": "Home Office",
@@ -25,6 +25,37 @@ print(data_eng_37.get("missing_key"))
 print(bool(data_eng_37.get("is this key here?")))
 data_eng_37.update({"course_length": "8 weeks"})
 print(data_eng_37)
+"""
 
-# TASK FOR TOMORROW
 # MAKE A DICTIONARY FOR YOUR FAVOURITE THINGS (FILMS, CARS ETC)
+film = {
+    "film_title": "The Shawshank Redemption",
+    "release_year": 1991,
+    "favourite": False,
+    "actor": {
+        "name": "Morgan Freeman",
+        "age": 84,
+        "test": {
+            "something": "Hello!"
+        }
+    }
+}
+
+print(bool(film.get("The Shawshank Redemption")))
+film.update({"age": 30})
+print(film["actor"]["age"])
+print(film["actor"]["test"]["something"])
+
+me = {'name': 'Munir', 'age': 22}
+print(me)
+print(type(me['name']))
+print(type(me['age']))
+print(f'{me["name"]}')
+print(f'My name is {me["name"]} and I am {me["age"]} years old.')
+
+mcu = {"Iron Man", "Thor", "Captain America", "Hulk"}
+print(mcu, type(mcu))  # type is a set
+# there is no order in a set, so running print on it will output in a different order every time
+
+fs = frozenset(mcu)
+# find out more about frozen sets
