da685a2d1bbbfaf1fae08371769978dae5a0557e
Munir Sharif <munirsharif463@gmail.com>
2022-05-24 10:41:57 +0100

    Completed FizzBuzz task and created functions_intro.py

--- control_flow/FizzBuzz.py
+++ control_flow/FizzBuzz.py
@@ -20,7 +20,7 @@ def fizz_buzz(stop):
     print(f'\nFizz: {fizz}\nBuzz: {buzz}\nFizzBuzz: {fizzbuzz}')
 
 
-keep_asking = True
+'''
 while keep_asking:  # while True...
     stop = input("What number would you like to FizzBuzz up to?\n")
     if stop.isdigit():  # if the input was a number...
@@ -28,3 +28,13 @@ while keep_asking:  # while True...
         keep_asking = False  # stop asking for a number if it's an int
     else:
         print("Please enter a valid number in digits.")
+'''
+
+play = True
+while play:
+    stop = input("What number would you like to FizzBuzz up to?\n")  # ask stopping number
+    fizz_buzz(int(stop) + 1)  # run fizzbuzz game
+    answer = input("Would you like to go again? (Y)es/ (N)o\n")  # get response
+    if answer.lower() in ("no", "n"):
+        play = False
+    # if answer isn't "n" then play again...
